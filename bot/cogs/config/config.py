181f789fc0ce466e3a4e36af51ed40b42b56f87a
tomo <tomohiro.togashi@gmail.com>
2024-01-08 21:58:57 +0900

    Revert "Refactor admin and members cogs"

--- bot/cogs/config/config.py
+++ bot/cogs/config/config.py
@@ -1,26 +1,25 @@
 import discord
 from discord.ext import commands
-from discord import Interaction, ui, SelectOption
 
 from components.view.self_introduction import SelfIntroductionButton
 from components.modals.self_introduction import SelfIntroductionModal
 
 
-class SampleViewA(ui.View):
+class SampleViewA(discord.ui.View):
     def __init__(self, bot: commands.Bot):
         super().__init__(timeout=180)
         self.bot = bot
 
     @discord.ui.select(
-        cls=ui.Select,
+        cls=discord.ui.Select,
         placeholder="What is your favorite fruit?",
         options=[
-            SelectOption(label="自己紹介", value="自己紹介"),
-            SelectOption(label="test", value="test"),
-            SelectOption(label="a", value="aaa"),
+            discord.SelectOption(label="自己紹介", value="自己紹介"),
+            discord.SelectOption(label="test", value="test"),
+            discord.SelectOption(label="a", value="aaa"),
         ],
     )
-    async def select(self, interaction: Interaction, select: ui.Select):
+    async def select(self, interaction: discord.Interaction, select: discord.ui.Select):
         print(select.values[0])
 
         if select.values[0] == "自己紹介":
